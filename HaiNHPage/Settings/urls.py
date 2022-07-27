edc1ba0bbb946f0d64a6e4ad9aa6f136a6de9dcf
Hai1998 <honghai98930@gmail.com>
2022-07-27 19:36:09 +0700

    push

--- HaiNHPage/Settings/urls.py
+++ HaiNHPage/Settings/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homeSetting, name='settings'),
-    path('addCommercial', views.addCommercial, name='addCommercial'),
+    path('addCommercial/', views.addCommercial, name='addCommercial'),
+    path('addCommercial/addRecordCommercial/', views.addRecordCommercial, name='addRecordCommercial'),
+    path('updateCommercial/<int:id>', views.updateCommercial, name='updateCommercial'),
+    path('updateCommercial/updateRecordCommercial/<int:id>', views.updateRecordCommercial, name='updateRecordCommercial'),
+    path('deleteCommercial/<int:id>', views.deleteCommercial, name='deleteCommercial'),
 ]
--- HaiNHPage/Settings/views.py
+++ HaiNHPage/Settings/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import loader
 from django.template import Template, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Commercials
+from django.urls import reverse
 
 
 # Create your views here.
 def homeSetting(request):
     commercials = Commercials.objects.all().values()
-    template = loader.get_template('settings/viewCommercial.html')
+    template = loader.get_template('settings/homeSetting.html')
     context = {
         'title': 'view',
         'commercials': commercials
@@ -16,5 +17,39 @@ def homeSetting(request):
 
 
 def addCommercial(request):
-    template = loader.get_template('settings/addCommercial.html')
+    template = loader.get_template('settings/homeSetting.html')
     return HttpResponse(template.render({}, request))
+
+
+def addRecordCommercial(request):
+    data = request.POST.getlist('commercial')
+    commercial = Commercials(first_text=data[0], second_text=data[1], thirst_text=data[2])
+    commercial.save()
+    return HttpResponseRedirect(reverse('settings'))
+
+
+def updateCommercial(request, id):
+    commercial = Commercials.objects.get(id=id)
+    template = loader.get_template('settings/homeSetting.html')
+    context = {
+        'title': 'update',
+        'commercial': commercial
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def updateRecordCommercial(request, id):
+    data = request.POST.getlist('commercial')
+    print(data)
+    commercial = Commercials.objects.get(id=id)
+    commercial.first_text = data[0]
+    commercial.second_text = data[1]
+    commercial.thirst_text = data[2]
+    commercial.save()
+    return HttpResponseRedirect(reverse('settings'))
+
+
+def deleteCommercial(request, id):
+    commercial = Commercials.objects.get(id=id)
+    commercial.delete()
+    return HttpResponseRedirect(reverse('settings'))
