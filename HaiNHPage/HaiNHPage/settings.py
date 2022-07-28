d64a6ba8f03e120ee6a9c67eaf631c01ec033c73
Hai1998 <honghai98930@gmail.com>
2022-07-28 07:36:16 +0700

    up

--- HaiNHPage/HaiNHPage/settings.py
+++ HaiNHPage/HaiNHPage/settings.py
@@ -127,9 +127,12 @@ os.path.join(BASE_DIR, "static"),
 
 STATIC_ROOT = ''
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- HaiNHPage/Settings/form.py
+++ HaiNHPage/Settings/form.py
@@ -1,7 +1,3 @@
-from django.forms import forms
 
 
-class CommercialForm(forms.Form):
-    first_text = forms.CharField(max_length=15)
-    second_text = forms.CharField(max_length=50)
-    thirst_text = forms.CharField(max_length=50)
+
